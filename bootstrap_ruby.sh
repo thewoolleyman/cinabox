5f84ac3acd425702b7a68c79d6c7b82f5e6e414c
Chad Woolley <thewoolleyman@gmail.com>
2008-07-16 22:56:19 -0700

    updates

--- bootstrap_ruby.sh
+++ bootstrap_ruby.sh
@@ -1,27 +1,28 @@
-# This script is designed to run on a standard Ubuntu Linux 7.04 Desktop install
+# This script is designed to run on a standard Ubuntu Linux Desktop install
 
-sudo apt-get update
+sudo aptitude update
 p 'installing zlib...'
-sudo apt-get install -y zlib1g zlib1g-dev
+sudo aptitude install -y zlib1g zlib1g-dev
 p 'installing ssl...'
-sudo apt-get install -y libssl-dev openssl
+sudo aptitude install -y libssl-dev openssl
 
+export RUBY_VERSION=1.8.6-p230
 export BUILD_DIR=~/build
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 
-rm -rf $BUILD_DIR/ruby-1.8.5.tar
+rm -rf $BUILD_DIR/ruby-$RUBY_VERSION.tar.gz
 
-wget ftp://ftp.ruby-lang.org/pub/ruby/ruby-1.8.5.tar.gz
-tar -zxvf ruby-1.8.5.tar.gz
+wget ftp://ftp.ruby-lang.org/pub/ruby/ruby-$RUBY_VERSION.tar.gz
+tar -zxvf ruby-$RUBY_VERSION.tar.gz
 
 # remove and uncomment all “non-Win” lines (all except
 # Win32API and win32ole)
-cp ruby-1.8.5/ext/Setup ruby-1.8.5/ext/Setup.orig
-cat ruby-1.8.5/ext/Setup.orig | grep -iv 'win' | sed -n -e 's/#\(.*\)/\1/p' > /tmp/Setup.new
-cp /tmp/Setup.new ruby-1.8.5/ext/Setup
+cp ruby-$RUBY_VERSION/ext/Setup ruby-$RUBY_VERSION/ext/Setup.orig
+cat ruby-$RUBY_VERSION/ext/Setup.orig | grep -iv 'win' | sed -n -e 's/#\(.*\)/\1/p' > /tmp/Setup.new
+cp /tmp/Setup.new ruby-$RUBY_VERSION/ext/Setup
 
-cd $BUILD_DIR/ruby-1.8.5
+cd $BUILD_DIR/ruby-$RUBY_VERSION
 
 ./configure
 make
