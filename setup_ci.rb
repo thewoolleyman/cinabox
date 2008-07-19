32096535b2ffd200f640f644bff1983ac10f9256
Chad Woolley <thewoolleyman@gmail.com>
2008-07-19 15:23:14 -0700

    adding command output

--- setup_ci.rb
+++ setup_ci.rb
@@ -18,58 +18,64 @@ class Cinabox
     FileUtils.cd(build_dir)
 
     # Install important packaages
-    `sudo apt-get install -y subversion`
-    `sudo apt-get install -y git-core git-svn`
-    `sudo apt-get install -y ssh`
+    run "sudo apt-get install -y subversion"
+    run "sudo apt-get install -y git-core git-svn"
+    run "sudo apt-get install -y ssh"
 
     # Download RubyGems if needed
     rubygems_mirror_id = '38646'
     unless File.exist?("rubygems-#{rubygems_version}.tgz")
-      `rm -rf rubygems-#{rubygems_version}.tgz`
-      `wget http://rubyforge.org/frs/download.php/#{rubygems_mirror_id}/rubygems-#{rubygems_version}.tgz`
+      run "rm -rf rubygems-#{rubygems_version}.tgz"
+      run "wget http://rubyforge.org/frs/download.php/#{rubygems_mirror_id}/rubygems-#{rubygems_version}.tgz"
     end
 
     # Force rubygems install/reinstall
     # TODO: Should try a gem update --system if RubyGems is already installed
-    unless `gem --version` =~ /#{rubygems_version}/
-      `rm -rf rubygems-#{rubygems_version}`
-      `tar -zxvf rubygems-#{rubygems_version}.tgz`
+    unless run "gem --version" =~ /#{rubygems_version}/
+      run "rm -rf rubygems-#{rubygems_version}"
+      run "tar -zxvf rubygems-#{rubygems_version}.tgz"
       FileUtils.cd "rubygems-#{rubygems_version}" do
-        `sudo ruby setup.rb`
+        run "sudo ruby setup.rb"
       end
     end
 
     # Install ccrb via git and dependencies
     unless File.exist?(ccrb_home)
-      `git clone #{ccrb_branch} #{ccrb_home}`
-      `sudo gem install rake mongrel_cluster`
+      run "git clone #{ccrb_branch} #{ccrb_home}"
+      run "sudo gem install rake mongrel_cluster"
     end
 
     # Always update ccrb
-    `cd #{ccrb_home} && git pull`
+    run "cd #{ccrb_home} && git pull"
     
     # TODO: Get ccrb_daemon pushed into ccrb trunk, then remove this
-    `cp #{cinabox_dir}/ccrb_daemon #{ccrb_home}/daemon/`
+    run "cp #{cinabox_dir}/ccrb_daemon #{ccrb_home}/daemon/"
     
     # Handle daemon setup
     unless File.exist?('/etc/ccrb')
-      `sudo mkdir -p /etc/ccrb`
-      `sudo chown #{current_user} /etc/ccrb`
+      run "sudo mkdir -p /etc/ccrb"
+      run "sudo chown #{current_user} /etc/ccrb"
     end
 
     unless File.exist?('/etc/ccrb/ccrb_daemon_config')
-      `echo "ENV['CCRB_USER']='#{current_user}'" > '/etc/ccrb/ccrb_daemon_config'`
-      `echo "ENV['CCRB_HOME']='#{ccrb_home}'" >> '/etc/ccrb/ccrb_daemon_config'`
+      run "echo "ENV['CCRB_USER']='#{current_user}'" > '/etc/ccrb/ccrb_daemon_config'"
+      run "echo "ENV['CCRB_HOME']='#{ccrb_home}'" >> '/etc/ccrb/ccrb_daemon_config'"
     end
     
     unless File.exist?('/etc/init.d/ccrb_daemon')
-      `sudo ln -f #{ccrb_home}/daemon/ccrb_daemon /etc/init.d/ccrb_daemon`
+      run "sudo ln -f #{ccrb_home}/daemon/ccrb_daemon /etc/init.d/ccrb_daemon"
     end
     
     unless File.exist?('/etc/rc3.d/S20cruise')
-      `sudo update-rc.d ccrb_daemon defaults`
+      run "sudo update-rc.d ccrb_daemon defaults"
     end
 
+  end
+  
+  def run(cmd)
+    puts "Running command: #{cmd}"
+    puts run "#{cmd}"
+    raise "Command failed: #{cmd}" unless $?.success?
   end
 end
 
