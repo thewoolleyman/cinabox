e6cd2edd062a09dcff9d5655f5fe073c54002ae4
Chad Woolley <thewoolleyman@gmail.com>
2009-05-09 13:31:47 -0700

    make create CI user have NOPASSWD sudo access

--- create_ci_user.sh
+++ create_ci_user.sh
@@ -7,11 +7,11 @@ sudo useradd -s /bin/bash -m -p `mkpasswd -H md5 $CI_PASSWORD` ci
 
 grep 'ci      ALL=(ALL)' /etc/sudoers
 if [ ! $? = 0 ]; then  
-  echo "  Giving ci user sudo privileges"
+  echo "  Giving ci user NO PASSWORD sudo privileges"
   rm -f /tmp/sudoers.tmp
   sudo cp /etc/sudoers /etc/sudoers.bak
   cp /etc/sudoers /tmp/sudoers.tmp
-  echo "ci      ALL=(ALL) ALL" >> /tmp/sudoers.tmp
+  echo "ci      ALL=(ALL) NOPASSWD: ALL" >> /tmp/sudoers.tmp
   visudo -q -c -s -f /tmp/sudoers.tmp
   if [ ! $? = 0 ]; then echo "error editing sudoers file" && exit; fi
   sudo cp /tmp/sudoers.tmp /etc/sudoers
