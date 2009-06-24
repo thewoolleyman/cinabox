dabfbf1d07fd5b34fe6d7706e2df4545ffb30dfc
Chad Woolley <thewoolleyman@gmail.com>
2009-06-24 04:01:49 -0700

    handle sudo echo problem

--- create_ci_user.sh
+++ create_ci_user.sh
@@ -17,7 +17,7 @@ if [ ! $? = 0 ]; then
   sudo rm -f /tmp/sudoers.tmp
   sudo cp /etc/sudoers /etc/sudoers.bak
   sudo cp /etc/sudoers /tmp/sudoers.tmp
-  sudo echo "ci      ALL=(ALL) NOPASSWD: ALL" >> /tmp/sudoers.tmp
+  sudo sh -c 'echo "ci      ALL=(ALL) NOPASSWD: ALL" >> /tmp/sudoers.tmp'
   if [ ! -e /tmp/sudoers.tmp ]; then echo "/tmp/sudoers.tmp does not exist, aborting before we blow away all sudo access and completely hose this system..." && exit 1; fi
   sudo visudo -q -c -s -f /tmp/sudoers.tmp
   if [ ! $? = 0 ]; then echo "error editing sudoers file" && exit; fi
