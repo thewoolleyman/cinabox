24dd6a23fb5a60c42484a2d127d3a036a5613681
Chad Woolley <thewoolleyman@gmail.com>
2009-07-22 04:02:32 -0700

    Disable Ruby program suffix until Rubygems issues with non-default format-executables are resolved.

--- bootstrap_ruby.sh
+++ bootstrap_ruby.sh
@@ -4,4 +4,4 @@
 
 wget -O /tmp/bootstrap_ruby.sh http://github.com/thewoolleyman/bootstrap-ruby/raw/master/bootstrap_ruby.sh
 chmod a+x /tmp/bootstrap_ruby.sh
-/tmp/bootstrap_ruby.sh
+NO_RUBY_PROGRAM_SUFFIX=true /tmp/bootstrap_ruby.sh # Disable Ruby program suffix until Rubygems issues with non-default format-executables are resolved.
