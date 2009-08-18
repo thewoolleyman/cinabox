bf8dfb94bdef0181f4c68cf36828531f6e6d134d
Chad Woolley <thewoolleyman@gmail.com>
2009-08-18 00:39:32 -0700

    allow override of bootstrap ruby download location, delete target file if it already exists

--- bootstrap_ruby.sh
+++ bootstrap_ruby.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # A wrapper script to download and run bootstrap_ruby.sh, see http://github.com/thewoolleyman/bootstrap-ruby
 
+  
+if [ -z $BOOTSTRAP_RUBY_GIT_DOWNLOAD_URL ]; then BOOTSTRAP_RUBY_GIT_DOWNLOAD_URL='http://github.com/thewoolleyman/bootstrap-ruby/raw/master'; fi
 
-wget -O /tmp/bootstrap_ruby.sh http://github.com/thewoolleyman/bootstrap-ruby/raw/master/bootstrap_ruby.sh
+sudo rm -f /tmp/bootstrap_ruby.sh
+wget -O /tmp/bootstrap_ruby.sh $BOOTSTRAP_RUBY_GIT_DOWNLOAD_URL/bootstrap_ruby.sh
 chmod a+x /tmp/bootstrap_ruby.sh
 NO_RUBY_PROGRAM_SUFFIX=true /tmp/bootstrap_ruby.sh # Disable Ruby program suffix until Rubygems issues with non-default format-executables are resolved.
