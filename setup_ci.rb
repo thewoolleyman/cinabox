a03786c5e33cf0c9c87f2970c58328065cef7373
Chad Woolley <thewoolleyman@gmail.com>
2009-08-18 21:49:07 -0700

    use CRUISE_HOME and CRUISE_USER instead of CCRB_HOME and CCRB_USER

--- setup_ci.rb
+++ setup_ci.rb
@@ -6,13 +6,13 @@ class Cinabox
     require 'socket'
 
     # Settings
-    ccrb_user = ENV['CCRB_USER'] || ENV['USER']
-    ccrb_home = ENV['CCRB_HOME'] || "#{ENV['HOME']}/ccrb"
+    cruise_user = ENV['CRUISE_USER'] || ENV['USER']
+    cruise_home = ENV['CRUISE_HOME'] || "#{ENV['HOME']}/ccrb"
     ccrb_branch = ENV['CCRB_BRANCH'] || "git://github.com/thoughtworks/cruisecontrol.rb.git"
     # ccrb_tag = ENV['CCRB_TAG']
     ccrb_tag = ENV['CCRB_TAG'] || 'v1.4.0' # Hardcode to 1.4.0 release until master branch is fixed
     cinabox_dir = File.expand_path(File.dirname(__FILE__))
-    ccrb_daemon_template = ENV['CCRB_DAEMON_TEMPLATE'] || "#{ccrb_home}/daemon/cruise"
+    ccrb_daemon_template = ENV['CCRB_DAEMON_TEMPLATE'] || "#{cruise_home}/daemon/cruise"
     
     # Build/download dir
     build_dir = ENV['BUILD_DIR'] || "#{ENV['HOME']}/build"
@@ -27,28 +27,28 @@ class Cinabox
     run "sudo aptitude install -y git-svn" if !((run "dpkg -l git-svn", false) =~ /ii  git-svn/) || force
 
     # Install ccrb via git and dependencies
-    if !File.exist?(ccrb_home) || force
-      run "rm -rf #{ccrb_home}"
-      run "git clone #{ccrb_branch} #{ccrb_home}"
-      run "cd #{ccrb_home} && git checkout -b #{ccrb_tag} refs/tags/#{ccrb_tag}" if ccrb_tag
+    if !File.exist?(cruise_home) || force
+      run "rm -rf #{cruise_home}"
+      run "git clone #{ccrb_branch} #{cruise_home}"
+      run "cd #{cruise_home} && git checkout -b #{ccrb_tag} refs/tags/#{ccrb_tag}" if ccrb_tag
       run "sudo gem install --bindir /usr/bin rake mongrel mongrel_cluster"
     end
 
     # Always update ccrb
     # TODO: disabled for now, it will break if we are on a tag
-    # run "cd #{ccrb_home} && git pull"
+    # run "cd #{cruise_home} && git pull"
     
     # Write out init script daemon based on template
     if !File.exist?('/etc/init.d/cruise') || force
       run "sudo rm -f /etc/init.d/cruise"
       run "sudo touch /etc/init.d/cruise"
-      run "sudo chown #{ccrb_user} /etc/init.d/cruise"
+      run "sudo chown #{cruise_user} /etc/init.d/cruise"
       run "chmod a+x /etc/init.d/cruise"
       File.open(ccrb_daemon_template, "r") do |input|
         File.open("/etc/init.d/cruise", "w") do |output|
           input.each_line do |line|
-            line = "CRUISE_USER = '#{ccrb_user}'\n" if line =~ /CRUISE_USER =/
-            line = "CRUISE_HOME = '#{ccrb_home}'\n" if line =~ /CRUISE_HOME =/
+            line = "CRUISE_USER = '#{cruise_user}'\n" if line =~ /CRUISE_USER =/
+            line = "CRUISE_HOME = '#{cruise_home}'\n" if line =~ /CRUISE_HOME =/
             output.print(line)
           end
         end
@@ -69,7 +69,9 @@ class Cinabox
       run "sudo aptitude install postfix -y"
     end
     
-    run "/etc/init.d/cruise start"
+    # Init script needs to be invoked via sudo from here, otherwise it may not have permission to create a pid file
+    # if this script was invoked by a user other than CRUISE_USER.  If the CRUISE_USER user runs it, you don't need sudo
+    run "sudo /etc/init.d/cruise start" 
 
     print "\n\nSetup script completed.\n"
   end
